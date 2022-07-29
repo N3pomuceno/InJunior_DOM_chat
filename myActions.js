6e116e00befc4f1d644cd0d13ad43af1ade5b43c
N3pomuceno <jnfazevedo@hotmail.com>
2022-07-29 01:02:30 -0300

    feat[js]: Botão

--- myActions.js
+++ myActions.js
@@ -2,18 +2,23 @@
 let btn1 = document.querySelector("#button1");
 btn1.addEventListener("click", function (){
     let texto = document.querySelector("#quadradoDeTexto");
-    let mensagem = document.createElement("div");
-    let container = document.getElementById("histórico");
-    mensagem.innerText = texto.value;
-    container.appendChild(mensagem);
-    mensagem.classList.add("mensagem");
-
-    let editar = document.createElement("button");
-    let excluir = document.createElement("button");
-    editar.classList.add("editar");
-    excluir.classList.add("excluir");
-    container.appendChild(editar);
-    container.appendChild(excluir);
-    editar.innerHTML = "Editar";
-    excluir.innerHTML = "Excluir";
+    if (texto.value == ""){
+        alert("Por favor adicione alguma mensagem!")
+    } else {
+        let mensagem = document.createElement("div");
+        let container = document.getElementById("histórico");
+        mensagem.innerText = texto.value;
+        container.appendChild(mensagem);
+        mensagem.classList.add("mensagem");
+    
+        //Botões
+        let editar = document.createElement("button");
+        let excluir = document.createElement("button");
+        editar.classList.add("editar");
+        excluir.classList.add("excluir");
+        container.appendChild(editar);
+        container.appendChild(excluir);
+        editar.innerHTML = "Editar";
+        excluir.innerHTML = "Excluir";
+    }
 })
