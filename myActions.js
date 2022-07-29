aa8796c344be93ff63432b371c38813852d45751
N3pomuceno <jnfazevedo@hotmail.com>
2022-07-29 12:42:08 -0300

    feat[botão]: Botão de excluir funcional

--- myActions.js
+++ myActions.js
@@ -20,5 +20,25 @@ btn1.addEventListener("click", function (){
         container.appendChild(excluir);
         editar.innerHTML = "Editar";
         excluir.innerHTML = "Excluir";
+
+        //Função de botões
+        editar.addEventListener("click", () => {
+            //Edição de mensagem
+            //Comando para pegar o que está dentro da mensagem.
+
+            //Comando para guardar informação do que está escrito no text box
+
+            //Comando para tacar o texto da mensagem no textbox
+
+            //Comando para alterar o 'enviar' do botão para 'editar'
+
+            //Fazer o processo de volta: pegar o que estava guardado e colocar de volta no textbox
+            //
+        })
+        excluir.addEventListener("click", () => {
+            excluir.remove();
+            editar.remove();
+            mensagem.remove();
+        })
     }
 })
